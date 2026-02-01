572f745d050a33a39972373de1fe534e20f373d9
scottw0173 <swarner5937@gmail.com>
2026-02-01 04:36:50 -0600

    added asteroid class, cheated to copy asteroidfield

--- main.py
+++ main.py
@@ -2,24 +2,35 @@ import pygame
 from logger import log_state
 import circleshape
 from player import Player
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
+from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2), PLAYER_RADIUS, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: 2.6.1 ")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    asteroid_field = AsteroidField()
+    player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2), PLAYER_RADIUS, 0)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill('black')
-        player.draw(screen)
-        player.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60))/1000
 
