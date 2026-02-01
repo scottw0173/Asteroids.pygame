d4d547365ec48b0df5ce0f619780f2bbd9028c57
scottw0173 <swarner5937@gmail.com>
2026-02-01 02:44:57 -0600

    created class for cirlces and preparing to draw first ship

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: 2.6.1 ")
     print(f'Screen width: {SCREEN_WIDTH}')
@@ -14,8 +16,8 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
-
-
+        clock.tick(60)
+        dt = (clock.tick(60))/1000
 
 if __name__ == "__main__":
     main()
